182e5c569a8c2b0f29c2d98d9e70673f9a69392c
quocanh5301 <82794519+quocanh5301@users.noreply.github.com>
2021-05-04 04:32:24 +0700

    Add files via upload

--- Book/src/main/java/Book/Book.java
+++ Book/src/main/java/Book/Book.java
@@ -1,23 +1,23 @@
 package Book;
 
 public class Book {
-//    public String name;
-//    public String author;
-//    public int numberOfPage;
-//    public static int numberOfBook;
-//    public Book(String name, String author, int numberOfPage){
-//        this.name = name;
-//        this.author = author;
-//        this.numberOfPage = numberOfPage;            
-//    }
-//    public String getName(Book book){
-//        String a = book.name;
-//        return a;
-//    }
-//    public int getNumberOfPage(Book book){
-//        int a = book.numberOfPage;
-//        return a;
-//    }        
+    public String name;
+    public String author;
+    public int numberOfPage;
+    public static int numberOfBook;
+    public Book(String name, String author, int numberOfPage){
+        this.name = name;
+        this.author = author;
+        this.numberOfPage = numberOfPage;            
+    }
+    public String getName(Book book){
+        String a = book.name;
+        return a;
+    }
+    public int getNumberOfPage(Book book){
+        int a = book.numberOfPage;
+        return a;
+    }        
 //    public setNumberOfPage(){
 //        
 //    }   
